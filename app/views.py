9f65ddf98b70ba96cec7ffc69a1c81be995a463c
Yash <yashpoojara268@gmail.com>
2021-04-23 16:17:29 +0530

    Completed Registration

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-
+from .models import *
 # Create your views here.
 
 
@@ -15,5 +15,17 @@ def RegisterUser(request):
         email = request.POST['email']
         password = request.POST['passwd']
         profile = request.FILES['profilepic']
-        print(role,email)
-        return redirect('/')
+
+        user = Admin.objects.filter(Email=email)
+        if len(user)>0:
+            msg = "User already exist"
+            return render(request, "app/index.html",{'err':msg})
+        else:
+            admin = Admin.objects.create(Role=role,Email=email,Password=password)
+            if role == 'Seller':
+                seller = Seller.objects.create(Admin=admin,Firstname=fname,Lastname=lname,Gender=gender,Profilepic=profile)
+            elif role == 'User':
+                user = User.objects.create(Admin=admin,Firstname=fname,Lastname=lname,Gender=gender,Profilepic=profile)
+            else:
+                print("Role Doesn't exist")
+            return redirect('/')
